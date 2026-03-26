ce5e55476acef482ea5a6117c81bab7b5b2ec03c
austintan <austintan2006@gmail.com>
2026-03-25 22:45:50 -0400

    feat: http server header + cmake

--- src/server/http_server/http_server.h
+++ src/server/http_server/http_server.h
@@ -4,5 +4,15 @@
 
 #ifndef DISTRIBUTED_APPEND_ONLY_LOG_HTTP_SERVER_H
 #define DISTRIBUTED_APPEND_ONLY_LOG_HTTP_SERVER_H
+#include "../../../vendor/httplib.h"
+#include "../append_log/append_log.h"
 
+class HttpServer {
+    AppendLog& log_;
+    httplib::Server http_server_;
+    public:
+        explicit HttpServer(AppendLog& log) : log_(log) {}
+        void start();
+        void stop();
+};
 #endif //DISTRIBUTED_APPEND_ONLY_LOG_HTTP_SERVER_H
