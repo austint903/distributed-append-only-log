6683e22a21bae1da771496a0435fc8e1b370adc8
austintan <austintan2006@gmail.com>
2026-04-07 23:43:41 -0400

    feat: node manager start/stop in main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,7 @@
 #include "udp_server/udp_server.h"
 #include "http_server/http_server.h"
 #include "topic_registry/topic_registry.h"
+#include "node_manager/node_manager.h"
 #include <thread>
 #include <csignal>
 #include <atomic>
@@ -21,9 +22,13 @@ int main() {
     HttpServer http(registry);
     std::thread http_thread([&]{ http.start(); });
 
+    NodeManager node_manager(registry);
+    node_manager.start();
+
     UdpServer server(*registry.get_or_create("default").log);
     server.run(running);
 
+    node_manager.stop();
     http.stop();
     http_thread.join();
     return 0;
