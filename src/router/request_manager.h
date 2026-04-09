d433608e1326a5b200c5fa557a4d75a0e7111ce0
austintan <austintan2006@gmail.com>
2026-04-09 00:49:49 -0400

    fix: read node return type to opt, can bubble up error

--- src/router/request_manager.h
+++ src/router/request_manager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "partition_map.h"
 #include "health_tracker.h"
+#include <optional>
 #include <string>
 
 class RequestManager {
@@ -8,7 +9,7 @@ public:
     RequestManager(PartitionMap& partition_map, HealthTracker& health_tracker);
 
     NodeInfo get_write_node(const std::string& topic);
-    NodeInfo get_read_node(const std::string& topic);
+    std::optional<NodeInfo> get_read_node(const std::string& topic);
 
 private:
     PartitionMap&  partition_map_;
