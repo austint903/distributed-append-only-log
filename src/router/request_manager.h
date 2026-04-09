4d44c7c701b4cdb294359034247b4d5385208230
austintan <austintan2006@gmail.com>
2026-04-09 00:35:48 -0400

    feat: request manager header - routes all requests to correct/healthy node

---
+++ src/router/request_manager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "partition_map.h"
+#include "health_tracker.h"
+#include <string>
+
+class RequestManager {
+public:
+    RequestManager(PartitionMap& partition_map, HealthTracker& health_tracker);
+
+    NodeInfo get_write_node(const std::string& topic);
+    NodeInfo get_read_node(const std::string& topic);
+
+private:
+    PartitionMap&  partition_map_;
+    HealthTracker& health_tracker_;
+};
