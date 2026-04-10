c09c25a00230d47d5b2e7922913c0139bf7b750a
austintan <austintan2006@gmail.com>
2026-04-09 23:54:32 -0400

    fix: update router header to take in health and request manager

--- src/router/router_server.h
+++ src/router/router_server.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "partition_map.h"
+#include "health_tracker.h"
+#include "request_manager.h"
 #include "../../vendor/httplib.h"
 
 class RouterServer {
 public:
-    RouterServer(PartitionMap& map, int port);
+    RouterServer(PartitionMap& map, std::vector<NodeInfo> all_nodes, int port);
     void start();
     void stop();
 
@@ -14,7 +16,9 @@ private:
     void forward_tail(const httplib::Request& req, httplib::Response& res);
     void handle_topics(const httplib::Request& req, httplib::Response& res);
 
-    PartitionMap& map_;
-    httplib::Server server_;
-    int port_;
+    PartitionMap&    map_;
+    HealthTracker    health_tracker_;
+    RequestManager   request_manager_;
+    httplib::Server  server_;
+    int              port_;
 };
